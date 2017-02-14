ee00e05bf222ca4369b4fdbd3dece9d053f088d9
Andrew Joslin <andrewtjoslin@gmail.com>
2017-02-13 18:15:58 -0700

    To es5

--- index.js
+++ index.js
@@ -7,29 +7,30 @@ var ValidationError = require('error/validation')
 var validatorStore = createStore()
 
 module.exports = function assertMyJsonValid (value, schema) {
-  const store = validatorStore(schema)
+  var store = validatorStore(schema)
 
-  const validate = store.validate ||
-        (store.validate = validator(schema, {verbose: true}))
+  var validate = store.validate ||
+    (store.validate = validator(schema, {verbose: true}))
 
   validate(value)
 
   if (validate.errors && validate.errors.length) {
-    throw ValidationError(validate.errors.map(mapError))
+    var error = mapError(validate.errors[0], schema)
+    throw ValidationError(error)
   }
+}
 
-  function mapError (error) {
-    return {
-      message: [
-        schema.title ? (schema.title + ':') : '',
-        error.type,
-        error.field,
-        error.message,
-        '. Value: ' +
-          (typeof error.value === 'object' ? JSON.stringify(error.value) : error.value)
-      ]
-        .filter(Boolean)
-        .join(' ')
-    }
+function mapError (error, schema) {
+  return {
+    message: [
+      schema.title ? (schema.title + ':') : '',
+      error.type,
+      error.field,
+      error.message,
+      '. Value: ' +
+        (typeof error.value === 'object' ? JSON.stringify(error.value) : error.value)
+    ]
+      .filter(Boolean)
+      .join(' ')
   }
 }
